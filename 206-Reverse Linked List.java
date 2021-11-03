79a393697cc0f3eaace873aba58974fdcbde1a5a
Alex-Liu-0919 <liuzhendell@gmail.com>
2021-11-03 11:41:12 -0600

    add ListNode class

--- 206-Reverse Linked List.java
+++ 206-Reverse Linked List.java
@@ -1,3 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
 // solution 1
 
 class Solution {
