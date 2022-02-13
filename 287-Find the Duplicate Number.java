45291052cac7ba43e2c4dfa6f57d36a88ef47a57
Alex-Liu-0919 <liuzhendell@gmail.com>
2022-02-12 23:04:44 -0700

    update

---
+++ 287-Find the Duplicate Number.java
@@ -0,0 +1,29 @@
+//二分法
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        int left = 1;
+        int right = nums.length - 1;
+        
+        while(left<right)
+        {
+            int count = 0;
+            int mid = (left + right)/2;
+            for(int num : nums)
+            {
+                if(num <= mid)
+                {
+                    count++;
+                }
+            }
+            if(count > mid)
+            {
+                right = mid;
+            }else{
+                left = mid+ 1;
+            }
+        }
+        return left;
+        
+    }
+}
